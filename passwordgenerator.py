fe36a54adeabe329524e0feb00f1ff1ba4f2a7e8
SamuelV1 <52385559+SamuelV1@users.noreply.github.com>
2022-07-08 19:06:00 -0300

    new options/ stronger passwords

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,10 +1,6 @@
 import random
 import string
 from random import randint
-#stash of letter
-LowerLetters = string.ascii_lowercase
-upperLetters = string.ascii_uppercase
-
 
 ## random punctuation
 def random_punctuation(y):
@@ -12,24 +8,40 @@ def random_punctuation(y):
 #function to get a especif number of random letters
 def random_char(y):
        return ''.join(random.choice(string.ascii_lowercase) for x in range(y))
+       
 def random_charup(y):
        return ''.join(random.choice(string.ascii_uppercase) for x in range(y))
        
-d = random_charup(random.randint(1,5))
-a = random_punctuation(random.randint(1,5))
-
-b = random_char(random.randint(1,6)) 
 # function to get random digit of numbers
 def random_with_N_digits(n):
     range_start = 10**(n-1)
     range_end = (10**n)-1
     return randint(range_start, range_end)
     
-c = random_with_N_digits(random.randint(1,5))
+
 # print(b+d+a+str(c))
 
 
 ## now function
-passwordLevel = int(input("password generator 1 for weak 2 for extra strong: "))
+passwordLevel = int(input("password generator 1 for simple 2 for extra strong: "))
 
 print(passwordLevel)
+#if the password is weak it will only have number and lowercase letters
+if passwordLevel == 1:
+    weak_leeter = random_char(random.randint(3,6)) 
+    weak_numbers = random_with_N_digits(random.randint(3,5))    
+    print (f"Here is your new password: {weak_leeter+str(weak_numbers)}")
+    #output example: pfloz2558
+    
+    #if the password is stronger it will have everything possible
+elif passwordLevel == 2:
+    
+    a = random_charup(random.randint(2,4))
+    b = random_punctuation(random.randint(3,4))
+    c = random_char(random.randint(1,4)) 
+    d = random_with_N_digits(random.randint(3,5))
+    print(f"Here is your newpassword: {c+b+a+str(d)}")
+    #example output: m.]]^WYQT6317
+    #Time to crack this password:
+    #  10 billion years
+    
