43e51ae9b8bd90317e24249db7245174ad267859
SamuelV1 <s4muelvic@outlook.com>
2022-07-16 12:16:07 -0300

    hangman done

--- tester.py
+++ tester.py
@@ -1,17 +1,24 @@
 # this is a playground codes here ARE NOT supossed to work or look pretty 
-word = "EVA"
-word2 = ["E","V", "A"]
+word = "EVARE"
+word2 = ["E","V","A","R","E",]
 placeholder = []
 c = len(word)
 print(c)
-
+print(*placeholder)
 print(word[1])
+
+
 
 for i in range(c):
     placeholder.append("_")
 new_word = "".join(word2)
+
+for i in range(len(indices)):
+    placeholder = placeholder[:indices[i]]+["E"]+placeholder[indices[i]+1:]
 print(new_word)
 print(*placeholder)
 
 if word == new_word:
     print("ok sim")
+    
+print(indices)
