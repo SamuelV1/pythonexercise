02a98fdf0c77842d11fe715be1a867659438432c
SamuelV1 <s4muelvic@outlook.com>
2022-07-15 23:59:51 -0300

    second phase

--- Hangman.py
+++ Hangman.py
@@ -1,4 +1,4 @@
-word = "evaporate"
+word = "eva"
 # ideais eu posso criar uma lista de _ do tamanho da palavra ai eu pego o index da letra e mudo o _ para a letra
 playing = True
 
@@ -7,11 +7,22 @@ placeholder = []
 for i in range(len(word)):
     placeholder.append("_")
 
-print(*placeholder)
+
+
+print("WELCOME TO HANGMAN")
 while playing:
     print("pick a letter")
+    print(*placeholder)
     user_guess = input("enter your letter: ")
     if user_guess in word:
-        print("ta porra menor")
-        playing = False
+        user_guess_index = word.index(user_guess)
+        #using slicing to replace the word
+        placeholder = placeholder[:user_guess_index]+[user_guess]+placeholder[user_guess_index+1:]
+        #checks if the user already guessed the whole word
+        if "".join(placeholder) == word:
+            print("CONGRATULATION")
+            print("thanks for playing")
+            playing = False
+        
+        
 
--- tester.py
+++ tester.py
@@ -1,14 +1,17 @@
 # this is a playground codes here ARE NOT supossed to work or look pretty 
-word = "evaporate"
+word = "EVA"
+word2 = ["E","V", "A"]
 placeholder = []
 c = len(word)
 print(c)
-d = word.index("p")
-print(d)
+
 print(word[1])
 
 for i in range(c):
     placeholder.append("_")
-    
+new_word = "".join(word2)
+print(new_word)
 print(*placeholder)
 
+if word == new_word:
+    print("ok sim")
