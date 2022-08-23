d749de0cf9b9902596bd8958b146976b24395e1c
SamuelV1 <52385559+SamuelV1@users.noreply.github.com>
2022-08-23 13:11:41 -0300

    Create ddd.py

---
+++ LP faculdade/ddd.py
@@ -0,0 +1,3 @@
+user = int(input())
+locais = {61: "Brasilia",71: "Salvador", 11:"Sao Paulo", 21: "Rio de Janeiro",32:"Juiz de Fora", 19: "Campinas", 27: "Vitoria", 31: "Belo Horizonte"}
+print(locais[user])
